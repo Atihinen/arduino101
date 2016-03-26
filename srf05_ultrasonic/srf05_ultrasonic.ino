236cb337f28cf66e867a3a94b1ad352a4572470e
Joonas Jauhiainen2 <joonas.jauhiainen@eficode.com>
2016-03-26 22:47:33 +0200

    Updated code.

--- srf05_ultrasonic/srf05_ultrasonic.ino
+++ srf05_ultrasonic/srf05_ultrasonic.ino
@@ -3,8 +3,12 @@
 #define LEDRED 7
 #define LEDGREEN 4
 
+int distance = -1;
+int pulse_in = 0;
+
 void setup(){
   Serial.begin(9600);
+  Serial.println("Ready");
   pinMode(ECHOPIN, INPUT);
   pinMode(TRIGPIN, OUTPUT);
   pinMode(LEDRED, OUTPUT);
@@ -17,19 +21,18 @@ void loop(){
   digitalWrite(TRIGPIN, HIGH);                  // Send a 10uS high to trigger ranging
   delayMicroseconds(10);
   digitalWrite(TRIGPIN, LOW);                   // Send pin low again
-  int distance = pulseIn(ECHOPIN, HIGH);        // Read in times pulse
-  distance = distance/58;  // Calculate distance from time of pulse
+  pulse_in = pulseIn(ECHOPIN, HIGH);        // Read in times pulse
+  distance = pulse_in/58;  // Calculate distance from time of pulse
+  Serial.println(distance);
   if (distance > 15){
-    Serial.println("Green");
+    //Serial.println("Green");
     digitalWrite(LEDRED, LOW);
     digitalWrite(LEDGREEN, HIGH);
   }
   else {
-    Serial.println("red");
+    //Serial.println("red");
     digitalWrite(LEDGREEN, LOW);
     digitalWrite(LEDRED, HIGH);
-  }
-                        
-  Serial.println(distance);                     
+  }                   
   delay(50);                                    // Wait 50mS before next ranging
 }
